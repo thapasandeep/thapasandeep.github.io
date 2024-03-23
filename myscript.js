4d313dab50e7df3ed11429bc7df0e95d3b008226
thapasandeep <thapa.sandeepth@gmail.com>
2024-03-23 11:44:44 +1100

    implement interactive function

---
+++ myscript.js
@@ -0,0 +1,11 @@
+
+// alert("I am looking at alert message");
+
+function showAlert() {
+    // alert("This alert box was called with the onload event Sandeep Thapa");
+    console.log( "Sandeep Thapa");
+}
+
+function saveData() {
+
+}

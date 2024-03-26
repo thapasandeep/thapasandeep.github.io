e529929ff6d5da227a90bece0114348b1f76e531
thapasandeep <thapa.sandeepth@gmail.com>
2024-03-26 12:16:26 +1100

    implement savedata

--- myscript.js
+++ myscript.js
@@ -7,5 +7,31 @@ function showAlert() {
 }
 
 function saveData() {
+    var lname = document.getElementById("lname").value;
+    var fname = document.getElementById("fname").value;
+    console.log(lname);
+    console.log(fname);
+}
+const btn = document.getElementById("submitButton")
+// document.getElementById("submitButton")
+if (btn) {
+    btn.addEventListener("click",function() {
+        console.log("Submit button is clicked");  
+    });
+} else {
+    console.log("Something is not Working");
+}
+// .addEventListener("click", function() {
+// {this.classList.add("blue-button");
+//     console.log("Submit button is clicked");
+// });
 
+// const input = document.querySelector("submitButton");
+const log = document.getElementById("submitButton");
+
+log.addEventListener("input", updateValue);
+
+function updateValue(e) {
+//   log.textContent = e.target.value;
+    console.log("roger");
 }
